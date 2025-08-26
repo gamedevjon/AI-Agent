e9d24b2b23ff9aba38eba8b6e74db6f0144afc1d
Jonathan Weinberger <jon@gamedevhq.com>
2025-08-25 20:54:39 -0400

    message history for user prompt

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 import sys
 
 load_dotenv()
@@ -9,21 +10,31 @@ api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
 def main():
+    check_verbose = False
 
-    if len(sys.argv) != 2:
+    if len(sys.argv) < 2:
         print("You need to include a prompt!")
         sys.exit(1)
+    if "--verbose" in sys.argv:
+        check_verbose = True
     #llm prompt
-    prompt = sys.argv[1]
+    user_prompt = sys.argv[1]
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
 
     response = client.models.generate_content(
-    model='gemini-2.0-flash-001', contents=prompt
+    model='gemini-2.0-flash-001',
+    contents=messages
     )
     print(response.text)
-    usage = response.usage_metadata
-
-    print(f"Prompt tokens: {usage.prompt_token_count}")
-    print(f"Response tokens: {usage.candidates_token_count}")
+    
+    if check_verbose == True:
+        usage = response.usage_metadata
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {usage.prompt_token_count}")
+        print(f"Response tokens: {usage.candidates_token_count}")
 
 if __name__ == "__main__":
     main()
